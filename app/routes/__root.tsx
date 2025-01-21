8e210cd9410b2e4e68668bbb62bfb93f2adf794a
Devlin Duldulao <webmasterdevlin@gmail.com>
2025-01-21 14:31:36 +0800

    feat: add favicon and apple touch icon links for improved branding

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -42,6 +42,25 @@ export const Route = createRootRouteWithContext<RootRouterContext>()({
           rel: 'stylesheet',
           href: globalStyle,
         },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/favicon-32x32.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/favicon-16x16.png',
+        },
+        { rel: 'manifest', href: '/site.webmanifest', color: '#fffff' },
+        { rel: 'icon', href: '/favicon.ico' },
       ],
     };
   },
