5bc30509c539c97b7627a34e47893e78ce64d91a
Devlin Duldulao <webmasterdevlin@gmail.com>
2025-02-06 23:57:50 +0800

    refactor: add error handling for getProject function in project service

--- app/data/services/project.ts
+++ app/data/services/project.ts
@@ -6,31 +6,35 @@ export async function getProject() {
 
   await slow(500);
 
-  const projects = await prisma.project.findMany({
-    include: {
-      teamMembers: true,
-    },
-  });
+  try {
+    const projects = await prisma.project.findMany({
+      include: {
+        teamMembers: true,
+      },
+    });
 
-  const project = projects.map((proj) => {
-    return {
-      ...proj,
-      teamMembers: proj.teamMembers.reduce(
-        (acc, member) => {
-          if (!acc[member.role]) {
-            acc[member.role] = { count: 0, members: [] };
-          }
-          acc[member.role].count += 1;
-          acc[member.role].members.push(member);
-          return acc;
-        },
-        {} as Record<
-          string,
-          { count: number; members: typeof proj.teamMembers }
-        >
-      ),
-    };
-  });
+    const project = projects.map((proj) => {
+      return {
+        ...proj,
+        teamMembers: proj.teamMembers.reduce(
+          (acc, member) => {
+            if (!acc[member.role]) {
+              acc[member.role] = { count: 0, members: [] };
+            }
+            acc[member.role].count += 1;
+            acc[member.role].members.push(member);
+            return acc;
+          },
+          {} as Record<
+            string,
+            { count: number; members: typeof proj.teamMembers }
+          >
+        ),
+      };
+    });
 
-  return project[0];
+    return project[0];
+  } catch (error) {
+    throw new Error('Prisma error. Might be from cold start');
+  }
 }
