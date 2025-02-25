b3e05d5e9362d8df37dd1120c9244b09743a5141
Devlin Duldulao <webmasterdevlin@gmail.com>
2025-02-25 09:10:33 +0100

    refactor: simplify error handling in getProject function and clean up code

--- app/data/functions/project.ts
+++ app/data/functions/project.ts
@@ -2,9 +2,5 @@ import { createServerFn } from '@tanstack/start';
 import { getProject } from '@/data/services/project';
 
 export const getProjectFn = createServerFn().handler(() => {
-  try {
-    return getProject();
-  } catch (error) {
-    throw new Error('Server Function error. Might be from cold start');
-  }
+  return getProject();
 });
